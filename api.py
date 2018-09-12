2f9d22b4a2b7a386d60c96d59ebd54e6c0348081
Samuel Aroney <sam@zooniverse.org>
2018-09-12 12:25:35 +0100

    Add initial user database

--- api.py
+++ api.py
@@ -1,5 +1,25 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+# Flask, Api and Resource are classses
+# reqparse is request parsing interface
 
 app = Flask(__name__)
 api = Api(app)
+
+users = [
+    {
+        "name": "Nicholas",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Jane",
+        "age": 50,
+        "occupation": "Occupational Therapist"
+    },
+    {
+        "name": "Steven",
+        "age": 35,
+        "occupation": "Builder"
+    },
+]
