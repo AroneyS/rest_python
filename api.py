a2139478dffd3a0dee620d864f86dab9aa7c4d82
Samuel Aroney <sam@zooniverse.org>
2018-09-12 12:45:22 +0100

    Add initial delete method

--- api.py
+++ api.py
@@ -70,3 +70,6 @@ class User(Resource):
             return user, 201
 
     def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted."format(name), 200
